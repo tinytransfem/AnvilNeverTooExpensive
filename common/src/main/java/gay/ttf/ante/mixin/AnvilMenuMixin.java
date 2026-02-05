3b95a8e1a1ea182f9ef10bbb3ef66260bf4a2563
tinytransfem <tinytransfem@gmail.com>
2026-02-05 22:06:23 +0100

    Fix mixins for Neoforge 1.21.5+

--- common/src/main/java/gay/ttf/ante/mixin/AnvilMenuMixin.java
+++ common/src/main/java/gay/ttf/ante/mixin/AnvilMenuMixin.java
@@ -7,12 +7,12 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(AnvilMenu.class)
 public class AnvilMenuMixin {
-    @ModifyConstant(method = "createResult", constant = @Constant(intValue = 40))
+    @ModifyConstant(method = "createResultInternal", constant = @Constant(intValue = 40))
     private int mixinLimitInt(int i) {
         return Integer.MAX_VALUE;
     }
 
-    @ModifyConstant(method = "createResult", constant = @Constant(intValue = 39))
+    @ModifyConstant(method = "createResultInternal", constant = @Constant(intValue = 39))
     private int mixinMaxInt(int i) {
         return Integer.MAX_VALUE - 1;
     }
